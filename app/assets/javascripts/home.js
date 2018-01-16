a7272a25498b8c571d34ea2d940da17bd16cabff
PK Banks <pkbanks12@gmail.com>
2018-01-15 19:55:21 -0500

    setting up pg for heroku, with controller and view

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,6 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+window.addEventListener('load', function(){
+  console.log('window loaded');
+})
